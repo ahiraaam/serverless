f4f7399b06fa7c8f6bb6edf1fc9bb37af52138d9
MarcoAlvarezg <50589501+MarcoAlvarezg@users.noreply.github.com>
2019-10-22 12:18:14 -0500

    Update format-entries.js

--- actions/format-entries.js
+++ actions/format-entries.js
@@ -7,11 +7,11 @@ function main(params) {
   // params contain the "rows" coming from Cloudant including the full documents
   return {
     entries: params.rows.map((row) => { return {
-      name: row.doc.name,
-	    email: row.doc.email,
-	    comment: row.doc.comment,
+      name: row.doc.nombre,
+	    email: row.doc.correo,
+	    comment: row.doc.comentario,
 	    createdAt: row.doc.createdAt,
-	    icon: (row.doc.email ? `https://secure.gravatar.com/avatar/${md5.hash(row.doc.email.trim().toLowerCase())}?s=64` : null)
+	    icon: (row.doc.correo ? `https://secure.gravatar.com/avatar/${md5.hash(row.doc.email.trim().toLowerCase())}?s=64` : null)
     }})
 	};
 }
